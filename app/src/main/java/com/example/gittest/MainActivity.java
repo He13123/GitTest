83709ceaa0533e03a66b4310742561468a4aa495
13233 <1323334891@qq.com>
2020-12-17 09:59:04 +0800

    第三次提交

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.gittest;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("aaaaa", "第一次更改: ");
+        Toast.makeText(this,"第一次更改",Toast.LENGTH_SHORT).show();
     }
-
-    private static final String TAG = "MainActivity";
 }
